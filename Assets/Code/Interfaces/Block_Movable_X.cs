57bb850eb155ee774e878de18ca12e64069cedba
DrNox08 <marcopergamena.01@gmail.com>
2024-12-22 16:15:24 +0100

    Hard Coded Working Drag on X

--- Assets/Code/Interfaces/Block_Movable_X.cs
+++ Assets/Code/Interfaces/Block_Movable_X.cs
@@ -1,6 +1,6 @@
 using System;
 using UnityEngine;
-using UnityEngine.EventSystems; // Necessario per le interfacce drag
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Block_Movable_X : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IBlock
@@ -11,8 +11,8 @@ public class Block_Movable_X : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     private bool isDragging = false;
     private Vector3 lastValidPosition;
 
-    private float maxX; // Limite massimo verso destra
-    private float minX; // Limite massimo verso sinistra
+    private float maxX;
+    private float minX;
 
     private void Awake()
     {
@@ -49,7 +49,6 @@ public class Block_Movable_X : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         else
         {
             rb.MovePosition(lastValidPosition);
-            
         }
     }
 
@@ -57,76 +56,62 @@ public class Block_Movable_X : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     {
         isDragging = false;
 
-        // Ottieni le dimensioni del blocco
         Vector3 halfExtents = blockCollider.bounds.extents;
-        float blockWidth = halfExtents.x * 2; // Larghezza totale del blocco (unità della griglia)
+        float blockWidth = halfExtents.x * 2;
 
-        // Calcola la posizione allineata
         Vector3 alignedPosition;
 
-        if (Mathf.RoundToInt(blockWidth) % 2 == 0) // Dimensione pari
+        if (Mathf.RoundToInt(blockWidth) % 2 == 0)
         {
-            // Calcola il bordo sinistro del blocco
-            float leftEdge = lastValidPosition.x - halfExtents.x;
-
-            // Arrotonda il bordo sinistro alla griglia
-            float alignedLeftEdge = Mathf.Round(leftEdge);
-
-            // Ricalcola il centro basandosi sul bordo sinistro allineato
-            float alignedCenterX = alignedLeftEdge + halfExtents.x;
-
-            // Imposta la nuova posizione allineata
-            alignedPosition = new Vector3(alignedCenterX, lastValidPosition.y, lastValidPosition.z);
+            float alignedX = RoundToNearestHalf(lastValidPosition.x);
+            alignedPosition = new Vector3(alignedX, lastValidPosition.y, lastValidPosition.z);
         }
-        else // Dimensione dispari
+        else
         {
-            // Arrotonda direttamente il centro alla griglia
             float alignedCenterX = Mathf.Round(lastValidPosition.x);
-
-            // Imposta la nuova posizione allineata
             alignedPosition = new Vector3(alignedCenterX, lastValidPosition.y, lastValidPosition.z);
         }
 
-        // Muovi il blocco alla posizione allineata
         rb.MovePosition(alignedPosition);
-
     }
-
-
-
 
     private void UpdateLimitsWithRaycast()
     {
         Vector3 halfExtents = blockCollider.bounds.extents;
-        
-        // Origin of raycasts
         Vector3 rightOrigin = transform.position + Vector3.right * halfExtents.x;
         Vector3 leftOrigin = transform.position + Vector3.left * halfExtents.x;
-        
-        // Raycast to the right
+
         if (Physics.Raycast(rightOrigin, Vector3.right, out RaycastHit hitRight, Mathf.Infinity))
         {
-            maxX = hitRight.point.x - halfExtents.x; // Subtract half width to avoid overlap
+            maxX = hitRight.point.x - halfExtents.x;
         }
         else
         {
-            maxX = Mathf.Infinity; // No obstacle
+            maxX = Mathf.Infinity;
         }
 
-        // Raycast to the left
         if (Physics.Raycast(leftOrigin, Vector3.left, out RaycastHit hitLeft, Mathf.Infinity))
         {
-            minX = hitLeft.point.x + halfExtents.x; // Add half width to avoid overlap
+            minX = hitLeft.point.x + halfExtents.x;
         }
         else
         {
-            minX = -Mathf.Infinity; // No obstacle
+            minX = -Mathf.Infinity;
         }
 
-        // Debug raycasts
         Debug.DrawRay(rightOrigin, Vector3.right * (maxX - rightOrigin.x), Color.green);
         Debug.DrawRay(leftOrigin, Vector3.left * (leftOrigin.x - minX), Color.red);
     }
 
-    
+    private float RoundToNearestHalf(float value)
+    {
+        float roundedValue = Mathf.Round(value * 2f) / 2f; // es: 1.47 *2 = 2.94, roundato 3, diviso 2 = 1.5 [ottengo multiplo di o.5]
+
+        if (Mathf.Abs(roundedValue % 1) < Mathf.Epsilon)
+        {
+            return value > roundedValue ? roundedValue + 0.5f : roundedValue - 0.5f;
+        }
+
+        return roundedValue;
+    }
 }
