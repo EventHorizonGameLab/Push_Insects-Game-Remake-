ea48973cf100fcdce9644411b704c315344d9dcc
emacrapa <emanuele.crapanzano02@gmail.com>
2025-01-16 20:33:50 +0100

    Door Animations implemented

--- Assets/Code/Blocks/Block_Door.cs
+++ Assets/Code/Blocks/Block_Door.cs
@@ -3,18 +3,38 @@ using UnityEngine;
 
 public class Block_Door : MonoBehaviour
 {
+    public enum DoorAnimation
+    {
+        Black,
+        Blue,
+        Green
+    }
+    [Header("Door Animation Settings")]
+    public DoorAnimation selectedAnimation; // Campo visibile dall'Inspector
+    private Animator animator;
     Rigidbody rb;
     GameObject keyChild;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        
+        animator = GetComponent<Animator>();
         keyChild = transform.GetChild(0).gameObject;
     }
     public void OpenDoor()
     {
         keyChild.SetActive(false);
-        gameObject.SetActive(false);
+        switch (selectedAnimation)
+        {
+            case DoorAnimation.Black:
+                animator.SetTrigger("Black");
+                break;
+            case DoorAnimation.Blue:
+                animator.SetTrigger("Blue");
+                break;
+            case DoorAnimation.Green:
+                animator.SetTrigger("Green");
+                break;
+        }
     }
 }
