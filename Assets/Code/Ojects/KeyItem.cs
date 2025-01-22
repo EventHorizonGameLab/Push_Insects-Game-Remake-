6cc557f8088f2783d5e9bea7991dcc25ef698993
marco pergamena <tuo@email.commarcopergamena.01@gmail.comgit config user.email tuo@email.commarcopergamena.01@gmail.com>
2025-01-22 21:04:46 +0100

    Fixed [can be merged]

--- Assets/Code/Ojects/KeyItem.cs
+++ Assets/Code/Ojects/KeyItem.cs
@@ -6,14 +6,21 @@ public class KeyItem : MonoBehaviour
 {
     [SerializeField] Block_Door doorParent;
     bool interacted;
+    bool checkAvailable;
 
     private void OnEnable()
     {
         WaitTween(1).OnComplete(() => interacted = false);
+        GameManager.OnMoveMade += CheckTime;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnMoveMade -= CheckTime;
     }
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") && GameManager.instance.PlayerIsDragging() == false && !interacted)
+        if (other.gameObject.CompareTag("Player") && GameManager.instance.PlayerIsDragging() == false && !interacted && checkAvailable)
         {
             doorParent.OpenDoor();
             Debug.Log("porta aperta");
@@ -22,10 +29,20 @@ public class KeyItem : MonoBehaviour
 
     }
 
- 
+
 
     Sequence WaitTween(float time)
     {
         return DOTween.Sequence().AppendInterval(time);
     }
+
+    void CheckTime()
+    {
+        WaitTween(0.5f).OnComplete(() =>
+        {
+            checkAvailable = true;
+            WaitTween(0.5f).OnComplete(() => checkAvailable = false);
+        });
+    }
+
 }
